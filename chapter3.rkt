b16c94060f09e69ef845474fcface40bcbbf4d0b
ss-gor <gor.martirosyan@sagittarius-systems.com>
2024-02-13 12:19:11 +0400

    Partially completed

--- chapter3.rkt
+++ chapter3.rkt
@@ -101,23 +101,7 @@
          (cdr l) 
          '())) 
    (define (iter a b) 
-     (cond ;((not (pair? a)) #f) 
-           ((not (pair? b)) #f) 
-           ((eq? a b) #t) 
-           ;((eq? a (safe-cdr b)) #t) 
-           (else (iter (safe-cdr a) (safe-cdr (safe-cdr b)))))) 
-   (iter lst (safe-cdr lst))) 
-  
-  
- ; Tested with mzscheme implementation of R5RS: 
- (define x '(1 2 3 4 5 6 7 8)) 
- (define y '(1 2 3 4 5 6 7 8)) 
- (set-cdr! (cdddr (cddddr y)) (cdddr y)) 
- (define z '(1)) 
- (set-cdr! z z) 
- x ; (1 2 3 4 5 6 7 8) 
- y ; (1 2 3 . #0=(4 5 6 7 8 . #0#)) 
- z ; #0=(1 . #0#) 
- (contains-cycle? x) ; #f 
- (contains-cycle? y) ; #t 
- (contains-cycle? z) ; #t 
+     (cond  ((not (pair? b)) #f) 
+            ((eq? a b) #t)  
+            (else (iter (safe-cdr a) (safe-cdr (safe-cdr b)))))) 
+   (iter lst (safe-cdr lst)))
